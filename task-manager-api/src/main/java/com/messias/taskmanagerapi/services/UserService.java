b9ca5edf63e456f83035bae31408f6bc0ac37713
Gabriel Rogério Messias <gamessiasjt@gmail.com>
2024-03-24 22:19:03 -0300

    Implementando buscar todos os usuários na camada de serviço | TaksManager

--- task-manager-api/src/main/java/com/messias/taskmanagerapi/services/UserService.java
+++ task-manager-api/src/main/java/com/messias/taskmanagerapi/services/UserService.java
@@ -1,7 +1,10 @@
 package com.messias.taskmanagerapi.services;
 
+import com.messias.taskmanagerapi.domain.User;
 import com.messias.taskmanagerapi.repositories.UserRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class UserService {
@@ -9,5 +12,9 @@ public class UserService {
 
     public UserService(UserRepository userRepository) {
         this.userRepository = userRepository;
+    }
+
+    public List<User> findAllUsers(){
+        return userRepository.findAll();
     }
 }
