60f1a9d9d23ae52c08b3fa9291fa0e372f298cfa
Gabriel Rogério Messias <gamessiasjt@gmail.com>
2024-03-24 22:15:11 -0300

    Configurando classe de serviço do usuário | TaksManager

--- task-manager-api/src/main/java/com/messias/taskmanagerapi/services/UserService.java
+++ task-manager-api/src/main/java/com/messias/taskmanagerapi/services/UserService.java
@@ -1,4 +1,13 @@
 package com.messias.taskmanagerapi.services;
 
+import com.messias.taskmanagerapi.repositories.UserRepository;
+import org.springframework.stereotype.Service;
+
+@Service
 public class UserService {
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
 }
