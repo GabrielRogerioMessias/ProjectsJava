2d238d4a617e061d2b7e36b8a67e5925ef1a50a0
Gabriel Rogério Messias <gamessiasjt@gmail.com>
2024-03-25 21:45:53 -0300

    Ajustando nome da classe | TaksManager

--- task-manager-api/src/main/java/com/messias/taskmanagerapi/controllers/exceptions/StandardError.java
+++ task-manager-api/src/main/java/com/messias/taskmanagerapi/controllers/exceptions/StandardError.java
@@ -8,7 +8,7 @@ import java.time.Instant;
 @NoArgsConstructor
 @Getter
 @Setter
-public class StandartError {
+public class StandardError {
     private Instant timestamp;
     private Integer status;
     private String error;
