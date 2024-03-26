ee6592fd1bd21fd6974518e09569d8874f3db3e9
Gabriel Rogério Messias <gamessiasjt@gmail.com>
2024-03-25 21:53:20 -0300

    Implementando o capturador de exceção para exceções no controller | TaksManager

---
+++ task-manager-api/src/main/java/com/messias/taskmanagerapi/controllers/exceptions/ControllerExceptionHandler.java
@@ -0,0 +1,24 @@
+package com.messias.taskmanagerapi.controllers.exceptions;
+
+import com.messias.taskmanagerapi.services.exceptions.UserAlreadyRegistered;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+
+import java.time.Instant;
+
+@ControllerAdvice
+public class ControllerExceptionHandler {
+
+    @ExceptionHandler(UserAlreadyRegistered.class)
+    public ResponseEntity<StandardError> userAlreadyRegistered(UserAlreadyRegistered e, HttpServletRequest request) {
+        String error = "User already registered";
+        HttpStatus status = HttpStatus.BAD_REQUEST;
+        StandardError err = new StandardError(Instant.now(), status.value(), error, e.getMessage(),
+                request.getRequestURI());
+        return ResponseEntity.status(status).body(err);
+    }
+}
