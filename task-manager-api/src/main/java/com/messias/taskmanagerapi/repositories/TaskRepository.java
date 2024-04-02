2632f8db459841822ad03bf635e32f4ebad777de
Gabriel Rogério Messias <gamessiasjt@gmail.com>
2024-04-01 21:14:38 -0300

    Buscar todas as categorias do usuário | Task Manager

--- task-manager-api/src/main/java/com/messias/taskmanagerapi/repositories/TaskRepository.java
+++ task-manager-api/src/main/java/com/messias/taskmanagerapi/repositories/TaskRepository.java
@@ -1,7 +1,15 @@
 package com.messias.taskmanagerapi.repositories;
 
 import com.messias.taskmanagerapi.domain.Task;
+import jakarta.persistence.criteria.CriteriaBuilder;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+import java.util.UUID;
 
 public interface TaskRepository extends JpaRepository<Task, Integer> {
+    @Query("SELECT T FROM Task T WHERE T.user = :idUser")
+    List<Task> allTaksByIdUser(@Param("idUser") UUID idUser);
 }
---
+++ task-manager-api/src/main/java/com/messias/taskmanagerapi/services/TaskService.java
@@ -0,0 +1,21 @@
+package com.messias.taskmanagerapi.services;
+
+import com.messias.taskmanagerapi.domain.Task;
+import com.messias.taskmanagerapi.repositories.TaskRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.UUID;
+
+@Service
+public class TaskService {
+    public TaskService(TaskRepository taskRepository) {
+        this.taskRepository = taskRepository;
+    }
+
+    private final TaskRepository taskRepository;
+
+    public List<Task> findAllTaks(UUID idUser) {
+        return taskRepository.allTaksByIdUser(idUser);
+    }
+}
