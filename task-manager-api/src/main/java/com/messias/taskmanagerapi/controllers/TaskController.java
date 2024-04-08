5710ce0d06a48f667ca35756b50ad9b0a13e473b
Gabriel Rogério Messias <gamessiasjt@gmail.com>
2024-04-07 21:20:43 -0300

    método inserir no controlador - task manager

--- task-manager-api/src/main/java/com/messias/taskmanagerapi/controllers/TaskController.java
+++ task-manager-api/src/main/java/com/messias/taskmanagerapi/controllers/TaskController.java
@@ -8,9 +8,14 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 import java.util.UUID;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @RestController
 @RequestMapping(value = "tasks")
@@ -32,4 +37,13 @@ public class TaskController {
         Task resulTask = taskService.findById(idTask);
         return ResponseEntity.ok().body(resulTask);
     }
+
+    @PostMapping
+    public ResponseEntity<Task> insert(@RequestBody Task newTask) {
+        taskService.insertNewTask(newTask);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{id}").buildAndExpand(newTask.getId())
+                .toUri();
+        return ResponseEntity.created(uri).body(newTask);
+    }
+
 }
