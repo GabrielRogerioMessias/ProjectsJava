cb39de0978810736d78043a4b219793a15d5b05d
Gabriel Rogério Messias <gamessiasjt@gmail.com>
2024-04-23 21:47:52 -0300

    criando teste unitário quando o método deletar categoria retorna uma exceção - task manager

--- task-manager-api/src/test/java/com/messias/taskmanagerapi/services/CategoryServiceTest.java
+++ task-manager-api/src/test/java/com/messias/taskmanagerapi/services/CategoryServiceTest.java
@@ -4,6 +4,7 @@ import com.messias.taskmanagerapi.domain.Category;
 import com.messias.taskmanagerapi.domain.User;
 import com.messias.taskmanagerapi.repositories.CategoryRepository;
 import com.messias.taskmanagerapi.repositories.UserRepository;
+import com.messias.taskmanagerapi.services.exceptions.ResourceNotFoundException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -55,13 +56,24 @@ class CategoryServiceTest {
 
     @Test
     @DisplayName("When category deletion was successfully ")
-    void deleteCategory() {
+    void deleteCategoryCase1() {
         Integer idCategory = 1;
         Category category = new Category(idCategory, "Category Test1");
         when(categoryRepository.findById(idCategory)).thenReturn(Optional.of(category));
         doNothing().when(categoryRepository).delete(category);
-        assertDoesNotThrow(()-> categoryService.delete(idCategory));
+        assertDoesNotThrow(() -> categoryService.delete(idCategory));
         verify(categoryRepository).delete(category);
         verify(categoryRepository).findById(idCategory);
+    }
+
+    @Test
+    @DisplayName("When category deletion returns a exception ")
+    void deleteCategoryCase2() {
+        Integer idCategory = 1;
+        when(categoryRepository.findById(idCategory)).thenReturn(Optional.empty());
+        assertThrows(ResourceNotFoundException.class, () -> categoryService.delete(idCategory));
+        verify(categoryRepository, times(1)).findById(idCategory);
+        verify(categoryRepository, never()).delete(any());
+
     }
 }
