0461916be8629b73a294fee851af2f29e2b37095
Gabriel Rogério Messias <gamessiasjt@gmail.com>
2024-05-29 20:16:04 -0300

    teste unitário para listar tarefas - task manager

--- task-manager-api/src/main/java/com/messias/taskmanagerapi/domain/Task.java
+++ task-manager-api/src/main/java/com/messias/taskmanagerapi/domain/Task.java
@@ -31,4 +31,6 @@ public class Task {
     @JoinColumn(name = "id_user")
     private User user;
 
+    public Task(String description) {
+    }
 }
--- task-manager-api/src/test/java/com/messias/taskmanagerapi/services/TaskServiceTest.java
+++ task-manager-api/src/test/java/com/messias/taskmanagerapi/services/TaskServiceTest.java
@@ -1,5 +1,12 @@
 package com.messias.taskmanagerapi.services;
 
+import com.messias.taskmanagerapi.domain.Category;
+import com.messias.taskmanagerapi.domain.Task;
+import com.messias.taskmanagerapi.domain.User;
+import com.messias.taskmanagerapi.domain.dtos.TaskDTO;
+import com.messias.taskmanagerapi.repositories.TaskRepository;
+import com.messias.taskmanagerapi.repositories.UserRepository;
+import com.messias.taskmanagerapi.utils.AuthenticatedUser;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -8,7 +15,13 @@ import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
 import com.messias.taskmanagerapi.repositories.CategoryRepository;
-import com.messias.taskmanagerapi.repositories.UserRepository;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.*;
 
 public class TaskServiceTest {
 
@@ -18,15 +31,39 @@ public class TaskServiceTest {
     private UserRepository userRepository;
     @Mock
     private CategoryRepository categoryRepository;
+    @Mock
+    private AuthenticatedUser authenticatedUser;
+    @Mock
+    private TaskRepository taskRepository;
+    User user = new User();
+    List<Category> categories;
+    List<Task> tasks;
 
     @BeforeEach
     public void setUp() {
         MockitoAnnotations.openMocks(this);
+        tasks = new ArrayList<>();
+        categories = new ArrayList<>();
+        Category category1 = new Category(1, "test");
+        Task task1 = new Task("test1");
+        Task task2 = new Task("test2");
+        tasks.add(task1);
+        tasks.add(task2);
+        categories.add(category1);
+        user.setUsername("username");
+        user.setCategoryList(categories);
     }
 
     @Test
-    @DisplayName("Testing the class")
-    void test() {
+    @DisplayName("When find all returns a list of tasks")
+    void findAllTasks() {
+        when(authenticatedUser.getCurrentUser()).thenReturn(user);
+        when(taskRepository.findAllTasks(user.getId())).thenReturn(tasks);
+        List<TaskDTO> result = taskService.findAllTasks();
+        assertEquals(tasks.get(1).getDescription(), result.get(1).description());
+        assertEquals(tasks.size(), result.size());
+        verify(authenticatedUser, times(1)).getCurrentUser();
+        verify(taskRepository, times(1)).findAllTasks(user.getId());
     }
 
 }
